c42bb3668b1ed05387df4ddb74491fadbf101816
gaocheng111111 <72428894+gaocheng111111@users.noreply.github.com>
2020-11-12 13:35:40 +0000

    fis

---
+++ Controller.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Controller : MonoBehaviour
+{
+    private NavMeshAgent meshAgent;
+    private List<GameObject> alltarget = new List<GameObject>();
+    private List<GameObject> TargetTypeslist = new List<GameObject>();
+    private float distance;
+    private int rand;
+    private int TargetCount;
+    private GameObject destination;
+    private bool moveAgain = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshAgent = gameObject.GetComponent<NavMeshAgent>();
+        PinkTarget();
+
+    }
+    void PinkTarget()
+    {
+        GameObject parentobject = GameObject.Find("Target");
+        TargetCount = parentobject.transform.childCount;
+
+        for (int i = 0; i < TargetCount; i++)
+        {
+            GameObject TargetType = parentobject.transform.GetChild(i).gameObject;
+            TargetTypeslist.Add(TargetType);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (destination != null)
+        {
+            distance = Vector3.Distance(gameObject.transform.position, destination.transform.position);
+        }
+
+        if (distance < 5)
+        {
+            moveAgain = true;
+        }
+
+        if(moveAgain == true)
+        { 
+            rand = UnityEngine.Random.Range(0, TargetCount);
+            destination = TargetTypeslist[rand];
+            meshAgent.SetDestination(destination.transform.position);
+            Debug.Log("Rand" + rand);
+            moveAgain = false;
+        }
+    }
+}
---
+++ allController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class allController : MonoBehaviour
+{
+    private List<GameObject> SpawnLocationlist = new List<GameObject>();
+    private List<GameObject> AgentTypeslist = new List<GameObject>();
+    private int AgentTypesCount;
+    public int AgentAllCount = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetAgentType();
+        GetSpawnLocation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnAgents();
+    }
+
+    void GetAgentType()
+    {
+        GameObject parentobject = GameObject.Find("AgentTypes");
+        AgentTypesCount = parentobject.transform.childCount;
+
+        for (int i = 0; i < AgentTypesCount; i++)
+        {
+            GameObject AgentType = parentobject.transform.GetChild(i).gameObject;
+            AgentTypeslist.Add(AgentType);
+        }
+    }
+    void GetSpawnLocation()
+    {
+        GameObject parentobject = GameObject.Find("SpawnLocation");
+        int SpawnLocationType = parentobject.transform.childCount;
+
+        for (int i = 0; i< SpawnLocationType; i++)
+        {
+            GameObject SpawnLocation = parentobject.transform.GetChild(i).gameObject;
+            SpawnLocationlist.Add(SpawnLocation);
+        }
+    }
+    void SpawnAgents()
+    {
+        int i = Random.Range(0, AgentTypesCount);
+        GameObject AgentSelected = AgentTypeslist[i];
+
+        int m = Random.Range(0, AgentTypesCount);
+        GameObject SpawnLocationSelected = SpawnLocationlist[m];
+
+        if (AgentTypeslist.Count<= AgentAllCount)
+        {
+            GameObject AgenttoSpawn = Instantiate(AgentSelected, SpawnLocationSelected.transform.position, SpawnLocationSelected.transform.rotation);
+            AgentTypeslist.Add(AgenttoSpawn);
+        }
+    }
+}
